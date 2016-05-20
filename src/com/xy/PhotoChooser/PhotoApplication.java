7d0a591d5d0e130b211151ee2bfb34cd446ab2af
liangyu@idstaff.com <liangyu@idstaff.com>
2016-05-20 12:16:22 +0800

    照片选择器，仿照微信，图片加载用glide，顺畅

---
+++ src/com/xy/PhotoChooser/PhotoApplication.java
@@ -0,0 +1,25 @@
+package com.xy.PhotoChooser;
+
+import android.content.Context;
+
+/**
+ * Description :
+ * Author : lauren
+ * Email  : lauren.liuling@gmail.com
+ * Blog   : http://www.liuling123.com
+ * Date   : 2016/3/4
+ */
+public class PhotoApplication extends android.app.Application {
+
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = this.getApplicationContext();
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+}
